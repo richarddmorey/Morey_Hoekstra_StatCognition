da2110eef28f9df78cfc97ca613c66c82d6af472
Richard D. Morey <richarddmorey@gmail.com>
2019-06-02 09:34:44 +0100

    Add basic webpage

---
+++ docs/js/task_pars.js
@@ -0,0 +1,24 @@
+var effect_size_names = [
+"whizbang balls",
+"constructo bricks",
+"rainbow clickers",
+"doodle noodles",
+"singing bling rings",
+"brahma buddies",
+"magic colorclay",
+"moon-candy makers"
+];
+
+var effect_sizes = {
+"whizbang balls"      : 0,
+"constructo bricks"	  : 0.10,
+"rainbow clickers"	  : 0.1850633,
+"doodle noodles"	  : 0.2961012,
+"singing bling rings" :	0.4331139,
+"brahma buddies"	  : 0.5961012,
+"magic colorclay"	  : 0.7850633,
+"moon-candy makers"	  : 1.00
+};
+
+
+var n = [10,12,14,16,19,22,26,30,35,41,48,57,66,78,91,106,125,146,171,200];
